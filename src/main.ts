879acfab607b5931c24eb05c32a8ff02f432ae03
Hoang Pham <lehoangpham1092@gmail.com>
2023-12-01 20:30:56 +0700

    Fixed lint issue.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { Date01Solution } from "./date-01/date_01";
 import { CustomFileReader } from "./file_reader";
+import { Date01Solution } from "./date-01/date_01";
 
 const fileReader = new CustomFileReader("src/date-01", "input.txt");
 const date01Sol = new Date01Solution(fileReader.read());
