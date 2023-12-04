c16ea1a0442eeb103503b7c3b2b2c127ea26d3cd
Hoang Pham <hoang.pham@team.enouvo.com>
2023-12-04 14:59:12 +0700

    finished date 04 part 01.

--- src/date-04/date_04_part_01_sol.ts
+++ src/date-04/date_04_part_01_sol.ts
@@ -4,25 +4,19 @@ import { SolutionData } from "../solution_data";
 export class Date04Part01Sol extends SolutionData implements Solution {
   solve(): number {
     const winningNumsRegex = /(?<=:\s+)\d.*(?=\s+\|)/g;
-    const ownedNumsRegex = /(?<=\|\s)\d.*$/g;
+    const ownedNumsRegex = /(?<=\|\s+)\d.*$/g;
     const wonNumsCounts: number[] = [];
     this.lines.forEach(line => {
       const winningNums = line.match(winningNumsRegex)?.[0].split(/\s+/).map(item => parseInt(item)) ?? [];
       const ownedNums = line.match(ownedNumsRegex)?.[0].split(/\s+/).map(item => parseInt(item)) ?? [];
-      console.log(winningNums);
-      console.log(ownedNums);
       
       let wonNumsCount = 0;
-      const wonNums: number[] = [];
       ownedNums.forEach(ownedNum => {
         if (winningNums.includes(ownedNum)) {
-          wonNums.push(ownedNum);
           wonNumsCount++;
         }
       });
-      console.log(wonNums);
       wonNumsCounts.push(wonNumsCount);
-      console.log("++++++++");
     });
     return wonNumsCounts.reduce((acc, current) => {
       if (current > 0) {
